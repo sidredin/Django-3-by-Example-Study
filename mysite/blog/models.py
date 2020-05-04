250f8d725586d3a8de2ab3ac7f58d542041c3e5f
dizelvinable <sadr3479@gmail.com>
2020-05-04 06:31:44 +0300

    Posts list and post detail views added

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -33,3 +34,9 @@ class Post(models.Model):
 
     objects = models.Manager()  # The default manager. If you declare any managers for your model but you want to keep the objects manager as well, you have to add it explicitly to your model.
     published = PublishedManager()  # Our custom manager.
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day, self.slug])
