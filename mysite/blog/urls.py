e2addb35d439868c437c8d160ebcdf7592fc84e7
dizelvinable <sadr3479@gmail.com>
2020-05-12 04:00:42 +0300

    Posts sharing system addea

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     # post views
     # path('', views.post_list, name='post_list'), # former view version (non class-based)
     path('', views.PostListView.as_view(), name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
-         views.post_detail,
-         name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
 ]
